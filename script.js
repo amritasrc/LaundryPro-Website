64f54570e431d046be91ccb9c235f20df59397ad
amritasrc <amritakumari13022006@gmail.com>
2026-07-03 13:59:01 +0530

    feat: implement add/remove service selection logic

---
+++ script.js
@@ -0,0 +1,96 @@
+
+
+const cart = [];
+
+const addbtn = document.querySelectorAll('.add-btn')
+const cartItems = document.getElementById('cartItems');
+const total = document.getElementById('totalAmount');
+const emptyRow = document.getElementById('emptyRow')
+
+addbtn.forEach(btn => {
+
+    btn.addEventListener('click', (e) => {
+
+        const serviceCard = e.currentTarget.closest(".service-card");
+
+        const name = serviceCard.dataset.name;
+        const price = Number(serviceCard.dataset.price);
+
+        const exist = cart.find(item => item.name === name)
+
+        if (exist) {
+            removeService(name);
+            return;
+        }
+
+        else {
+            cart.push({
+                name,
+                price,
+            });
+        }
+
+
+        console.log("1111111111");
+        console.log(cart);
+
+        btn.classList.remove("add-btn");
+        btn.classList.add("remove-btn");
+
+        btn.innerHTML = "&mdash; Remove Item";
+
+        renderCart();
+
+    });
+
+});
+
+const removeService = (name) => {
+
+    const index = cart.findIndex(item => item.name === name);
+
+    if (index !== -1) {
+        cart.splice(index, 1);
+    }
+
+    document.querySelectorAll('.service-card').forEach(card => {
+
+        if (card.dataset.name === name) {
+
+            const btn = card.querySelector("button");
+
+            btn.classList.remove('remove-btn');
+            btn.classList.add('add-btn')
+
+            btn.innerHTML = "+ Add Item"
+        }
+    })
+
+    console.log("222222222");
+    console.log(cart);
+    
+
+    renderCart();
+}
+
+const renderCart = () => {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// (function () {
+//     emailjs.init({
+//         publicKey: "itx9JUGEKzunqJI45",
+//     });
+// })();
+
+//  emailjs.send("service_t6n0y5i", "template_5sb54jc", templateParams);
